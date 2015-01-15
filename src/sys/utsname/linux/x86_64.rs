bb42286f8587db91d7076d449c7488e18317e709
Nicolas Kirchner <nicolas.kirchner@telecomnancy.net>
2015-01-15 20:28:00 +0100

    Suppression des suffixes us inutiles

--- src/sys/utsname/linux/x86_64.rs
+++ src/sys/utsname/linux/x86_64.rs
@@ -1,12 +1,12 @@
 #[repr(C)]
 #[derive(Copy)]
 pub struct utsname {
-    pub sysname: [::char_t; 65us],
-    pub nodename: [::char_t; 65us],
-    pub release: [::char_t; 65us],
-    pub version: [::char_t; 65us],
-    pub machine: [::char_t; 65us],
-    __domainname: [::char_t; 65us],
+    pub sysname: [::char_t; 65],
+    pub nodename: [::char_t; 65],
+    pub release: [::char_t; 65],
+    pub version: [::char_t; 65],
+    pub machine: [::char_t; 65],
+    __domainname: [::char_t; 65],
 }
 
 new!(utsname);
